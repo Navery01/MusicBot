a9a756a756f1268012842053a6a98056d7de079f
NVAvery <nvavery@gmail.com>
2025-02-07 23:26:32 -0600

    InitialTokenSystem

---
+++ MusicBot/db_service.py
@@ -0,0 +1,139 @@
+import mysql.connector as mysql
+from mysql.connector import errorcode
+
+
+
+from MusicBot.configs import CONFIG
+
+
+class DBService:
+    def __init__(self):
+
+        self.db = mysql.connect(**CONFIG['mysql'])
+
+    
+
+    def create_table(self, table_name, columns):
+        cursor = self.db.cursor()
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({columns});")
+        cursor.close()
+
+        self.db.commit()
+
+    def add_user(self, guild_id, user_id, user_name):
+        cursor = self.db.cursor()
+        try:
+            if self.get_user(guild_id, user_id):
+                print('[SQL] User already exists')
+                return
+            else:
+                cursor.execute("INSERT INTO users (user_id, user_name, guild_id) VALUES (%s, %s, %s);", (user_id, user_name, guild_id))
+                print('[SQL] User added')
+
+        except mysql.Error as err:
+            if err.errno == errorcode.ER_NO_SUCH_TABLE:
+                print(f"Error: Table 'users' does not exist.")
+                self.create_table('users', 'user_id BIGINT, user_name VARCHAR(64), guild_id BIGINT, load_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP')
+            else:
+                print(err.msg)
+        cursor.close()
+
+        self.db.commit()
+
+    def get_user(self, guild_id, user_id):
+        cursor = self.db.cursor()
+        cursor.execute("SELECT * FROM users WHERE user_id = %s AND guild_id = %s;", (user_id, guild_id))
+        user = cursor.fetchone()
+        cursor.close()
+        return user
+    
+    def add_guild(self, guild_id, guild_name):
+        cursor = self.db.cursor()
+        try:
+            if self.get_guild(guild_id):
+                print('[SQL] Guild already exists')
+                return
+            else:
+                cursor.execute("INSERT INTO guilds (guild_id, guild_name) VALUES (%s, %s);", (guild_id, guild_name))
+                print('[SQL] Guild added')
+
+        except mysql.Error as err:
+            if err.errno == errorcode.ER_NO_SUCH_TABLE:
+                print(f"Error: Table 'guilds' does not exist.")
+                self.create_table('guilds', 'guild_id BIGINT, guild_name VARCHAR(64), load_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP')
+            else:
+                print(err.msg)
+        cursor.close()
+
+        self.db.commit()
+    
+    def get_guild(self, guild_id):
+        cursor = self.db.cursor()
+        cursor.execute("SELECT * FROM guilds WHERE guild_id = %s;", (guild_id,))
+        guild = cursor.fetchone()
+        cursor.close()
+        return guild
+    
+    def init_points(self, guild_id, user_id):
+        cursor = self.db.cursor()
+        cursor.execute("INSERT INTO points (user_id, guild_id, points) VALUES (%s, %s, 100);", (user_id, guild_id))
+        print('[SQL] Points added')
+        cursor.close()
+        self.db.commit()
+
+
+    def add_points(self, guild_id, user_id, points):
+        cursor = self.db.cursor()
+        try:
+            if self.get_points(guild_id, user_id):
+                cursor.execute("UPDATE points SET points = points + %s WHERE user_id = %s AND guild_id = %s;", (points, user_id, guild_id))
+                print('[SQL] Points updated')
+            else:
+                self.init_points(guild_id, user_id)
+                cursor.execute("UPDATE points SET points = points + %s WHERE user_id = %s AND guild_id = %s;", (points, user_id, guild_id))
+                print('[SQL] Points updated')
+
+        except mysql.Error as err:
+            if err.errno == errorcode.ER_NO_SUCH_TABLE:
+                print(f"Error: Table 'points' does not exist.")
+                self.create_table('points', 'user_id BIGINT, guild_id BIGINT, points INT, load_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP')
+            else:
+                print(err.msg)
+        cursor.close()
+
+        self.db.commit()
+    
+    def get_points(self, guild_id, user_id):
+        try:
+            cursor = self.db.cursor()
+            print('[SQL] Getting points')
+            cursor.execute("SELECT points FROM points WHERE user_id = %s AND guild_id = %s;", (user_id, guild_id))
+            points = cursor.fetchone()
+            cursor.close()
+            if not points:
+                print('[SQL] No points found')
+                print('[SQL] Initializing points')
+                self.init_points(guild_id, user_id)
+                return 100
+        except mysql.Error as err:
+            print(err.msg)
+            return None
+        return points[0]
+    
+    def update_points(self, guild_id, user_id, points):
+        num_pts = self.get_points(guild_id, user_id)
+        if num_pts < points:
+            return
+        
+        cursor = self.db.cursor()
+        print('[SQL] Updating points')
+        cursor.execute("UPDATE points SET points = points + %s WHERE user_id = %s AND guild_id = %s;", (points, user_id, guild_id))
+        print('[SQL] Points updated')
+        cursor.close()
+        self.db.commit()
+        return True
+
+
+if __name__ == '__main__':
+    db = DBService()
+    print(db.add_points(1335302010374848623, 183987232359579650, 100))
---
+++ MusicBot/token_cog.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import random
+from MusicBot.db_service import DBService
+
+class token_cog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.db = DBService()
+        print('[Token Cog] Initialized')
+
+    @commands.command(name="roll", aliases=["dr"], help="starts a deathroll game")
+    async def roll(self, ctx, max_roll: int = 1000):
+        roll = random.randint(1, max_roll)
+        await ctx.send(f'{ctx.author.name} : **{roll}** [0, {max_roll}]')
+    
+    @commands.command(name="flip", aliases=["coin"], help="flips a coin")
+    async def flip(self, ctx):
+        flip = random.choice(['Heads', 'Tails'])
+        await ctx.send(f'{ctx.author.name} : **{flip}**')
+    
+    @commands.command(name='points', aliases=['point', 'pts'], help='Displays the points of the user')
+    async def points(self, ctx):
+        num_pts = self.db.get_points(ctx.guild.id, ctx.author.id)
+        if num_pts is None:
+            await ctx.send('No points found')
+            return
+        await ctx.send(f'{ctx.author.name} has {num_pts} points')
+
+    @commands.command(name='give', aliases=['gift'], help='Gives points to another user')
+    async def give(self, ctx, user: discord.Member, points: int):
+        if points < 0:
+            await ctx.send('Cannot give negative points')
+            return
+        num_pts = self.db.get_points(ctx.guild.id, ctx.author.id)
+        if num_pts < points:
+            await ctx.send('Not enough points')
+            return
+        
+        self.db.add_points(ctx.guild.id, user.id, points)
+        self.db.add_points(ctx.guild.id, ctx.author.id, -points)
+        await ctx.send(f'{ctx.author.name} gave {points} points to {user.name}')
+    
